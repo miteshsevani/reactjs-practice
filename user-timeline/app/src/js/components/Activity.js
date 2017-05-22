e0bf59cabbb82cfb1ddb857b4a41ee8cd980b01f
miteshsevani <msevani@gmail.com>
2017-05-22 13:26:47 +0100

    Built activity array and displayed using map

--- user-timeline/app/src/js/components/Activity.js
+++ user-timeline/app/src/js/components/Activity.js
@@ -3,30 +3,36 @@ import React from "react";
 export default class Activity extends React.Component {
     render() {
         
-        const activity = this.props.activity;
+        const {activities} = this.props;
 
         return(
-            <div className="activity">
-                
-                <div className="divider"></div>
-                
-                <div className="item">
-                    <div className="avatar">
-                        <img className="user-image" src={activity.user.avatar} />
-                        <div className="user-name">{activity.user.name}</div>
+            <div>
+            {activities.map((activity) => {
+                return(
+                    <div key={activity.user.id} className="activity">
+                        
+                        <div className="divider"></div>
+                        
+                        <div className="item">
+                            <div className="avatar">
+                                <img className="user-image" src={activity.user.avatar} />
+                                <div className="user-name">{activity.user.name}</div>
+                            </div>
+                        </div>
+
+                        <div className="description">
+                            <p className="time">{activity.time}</p>    
+                            <p className="post">{activity.post}</p>    
+                        </div>
+                        
+                        <div className="comments">
+                            <i className="fa fa-comment"></i>
+                            <span>{activity.comments.length}</span>
+                        </div>
+
                     </div>
-                </div>
-
-                <div className="description">
-                    <p className="time">{activity.time}</p>    
-                    <p className="post">{activity.post}</p>    
-                </div>
-                
-                <div className="comments">
-                    <i className="fa fa-comment"></i>
-                    <span>{activity.comments.length}</span>
-                </div>
-
+                );
+            })}
             </div>
         );
     }
--- user-timeline/app/src/js/components/UserTimeline.js
+++ user-timeline/app/src/js/components/UserTimeline.js
@@ -5,24 +5,40 @@ import Activity from "./Activity";
 export default class UserTimeline extends React.Component {
 
     render() {
-        const data = {
-            time: "An hour ago",
-            user: {
-                id: 1,
-                name: "Doug",
-                avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+        const data = [
+            {
+                time: new Date().getTime(),
+                user: {
+                    id: 1,
+                    name: "Doug",
+                    avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+                },
+                post: "Just had lunch",
+                comments: [
+                    {from: "Ari",text: "Me too!"},
+                    {from: "Greg",text: "Yum!"},
+                ]
             },
-            post: "Just had lunch",
-            comments: {
-                from: "Ari",
-                text: "Me too!"
-            }
-        }
+            {
+                time: new Date().getTime(),
+                user: {
+                    id: 2,
+                    name: "Doug",
+                    avatar: "http://www.croop.cl/UI/twitter/images/doug.jpg"
+                },
+                post: "Time for work",
+                comments: [
+                    {from: "Tom",text: "Boring..."},
+                    {from: "Liz",text: "Where do you work?"},
+                    {from: "Liz",text: "When do you finish"},
+                ]
+            },
+        ];
 
         return(
             <div>
                 <Header />
-                <Activity activity={data} />
+                <Activity activities={data} />
             </div>
         );
     }
