605615f95b7ea87f3c029c44bd125cd0ef1391bd
miteshsevani <msevani@gmail.com>
2017-07-25 17:45:22 +0100

    Completed redux basic intro

--- learn-redux/app/src/js/redux-example.js
+++ learn-redux/app/src/js/redux-example.js
@@ -1,123 +1,35 @@
 const redux = require('redux');
-
-// Name reducer and action generators
-// --------------------
-const nameReducer = (state = 'Anonymous', action) => {
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return action.name;            
-        default:
-            return state;
-    }
-}
-
-
-// Hobbies reducer and action generators
-// --------------------
-let nextHobbyId = 1;
-const hobbiesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_HOBBY':
-            return [
-                ...state,
-                {
-                    id: nextHobbyId++,
-                    hobby: action.hobby
-                }
-            ];        
-        case 'REMOVE_HOBBY':
-            return {                
-                hobbies: state.filter((hobby) => hobby.id !== action.id)
-            };
-        default:
-            return state;
-    }
-}
-
-
-// Movies reducer and action generators
-// --------------------
-let nextMovieId = 1;
-const moviesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_MOVIE':
-            return [
-                ...state,
-                {
-                    id: nextMovieId++,
-                    title: action.title,
-                    genre: action.genre
-                }
-            ];        
-        case 'REMOVE_MOVIE':
-            return {                
-                movies: state.filter((movie) => movie.id !== action.id)
-            };
-        default:
-            return state;
-    }
-}
-
-const reducer = redux.combineReducers({
-    name: nameReducer,
-    hobbies: hobbiesReducer,
-    movies: moviesReducer
-
-})
-
-const store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-));
+const actions = require('./actions/index');
+const store = require('./store/configureStore').configure();
 
 // subcribe to changes
 const unsubscribe = store.subscribe(() => {
     const state = store.getState();    
-    document.getElementById('app').innerHTML = state.name;
+    console.log('New state', store.getState());
 
-    console.log('New state', state);
+    if(state.isFetching) {
+        document.getElementById('app').innerHTML = "Loading";
+
+    } else if (state.map.url) {
+        document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View your location</a>';
+    }
 });
 
 //unsubscribe();
 
+// Name
+store.dispatch(actions.changeName('Mitesh'));
+store.dispatch(actions.changeName('Heena'));
 
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Mitesh'
-});
+// Hobby
+store.dispatch(actions.addHobby('Running'));
+store.dispatch(actions.addHobby('Swimming'));
+store.dispatch(actions.removeHobby(2));
 
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Tom'
-});
+// Movie
+store.dispatch(actions.addMovie('The Gladiator','Action'));
+store.dispatch(actions.addMovie('The Green Mile','Drama'));
+store.dispatch(actions.removeMovie(1));
 
-store.dispatch({
-    type: 'ADD_HOBBY',
-    hobby: 'Running'
-});
-
-store.dispatch({
-    type: 'ADD_HOBBY',
-    hobby: 'Swimming'
-});
-
-store.dispatch({
-    type: 'REMOVE_HOBBY',
-    id: 2
-});
-
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: 'The Gladiator',
-    genre: 'Action'
-});
-
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: 'The Green Mile',
-    genre: 'Drama'
-});
-
-store.dispatch({
-    type: 'REMOVE_MOVIE',
-    id: 1
-});
+// Maps
+store.dispatch(actions.fetchLocation());
