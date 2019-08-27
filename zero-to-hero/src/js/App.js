2774986564cb709a2e9445f512b79a2592e1fcf8
miteshsevani <msevani@gmail.com>
2019-08-27 12:12:57 +0100

    Removed redundant react projects

--- zero-to-hero/src/js/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Main from "./components/Main";
-
-const app = document.getElementById("app");
-
-ReactDOM.render(<Main />,app);
