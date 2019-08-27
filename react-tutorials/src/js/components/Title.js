568a65fa65d8a8f77d5c97ac1107d734b5b224e6
miteshsevani <msevani@gmail.com>
2019-08-27 12:19:20 +0100

    Created react bootstrap folder

--- react-tutorials/src/js/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Title extends React.Component {
-	render() {
-		return (			
-			<h1 onClick={this.props.changeTitle}>{this.props.title}</h1>
-		);
-	}
-}
